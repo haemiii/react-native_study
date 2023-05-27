a8485e0250dc40993208d00d2fcb65bfb30b41d5
강혜미 <eham1003@naver.com>
2023-05-27 19:40:08 +0900

    touchables

--- WorkHardStudyHardApp/App.js
+++ WorkHardStudyHardApp/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  header : {
+    flexDirection: "row",
+    justifyContent : "space-between",
+    alignItems : "center",
+    width : "90%",
+    marginTop : 100,
+  } ,
+  btnText : {
+    color : "white",
+    fontSize : 50,
+  }
 });
