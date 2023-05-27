8a183eda21add3b9fde39762e4f4e1fdf51e66bb
강혜미 <eham1003@naver.com>
2023-05-27 20:16:15 +0900

    create To Dos

--- WorkHardStudyHardApp/App.js
+++ WorkHardStudyHardApp/App.js
@@ -6,10 +6,20 @@ import { theme } from './colors';
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({}); //hashmap
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload)=>{
     setText(payload)
+  }
+  const addToDo=()=>{
+    if(text === ""){
+      return 
+    }
+    // save to do
+    const newToDos = Object.assign({}, toDos, {[Date.now()] : {text, work:working}})
+    setToDos(newToDos)
+    setText("");
   }
   return (
     <View style={styles.container}>
@@ -25,7 +35,9 @@ export default function App() {
       <View>
         <TextInput 
         // keyboardType='number-pad'
+        onSubmitEditing={addToDo}
         onChangeText={onChangeText}
+        value={text}
         placeholder={working ? "Add a To Do" : "Where do you want to go?"} style={styles.input}></TextInput>
       </View>
     </View>
